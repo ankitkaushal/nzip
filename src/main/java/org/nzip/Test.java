717b2015a3bdae0f5029c7c3ea6c999043f08ac5
ankitkaushal <ankit.kaushal.666@gmail.com>
2018-09-09 17:20:34 +0100

    testing on local

--- src/main/java/org/nzip/Test.java
+++ src/main/java/org/nzip/Test.java
@@ -3,7 +3,10 @@ package org.nzip;
 import java.io.File;
 
 public class Test {
+
+	int testvar=2;
 public static void main(String[] args) {
+
 	System.out.println(File.separator);
 	
 	String s = "a\\b";
