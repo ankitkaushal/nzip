2fd8444c6b92a9ef5a2a6344c1395d511c5eecfe
ankitkaushal <ankit.kaushal.666@gmail.com>
2018-09-09 17:19:14 +0100

    deleted line and added var

--- src/main/java/org/nzip/Test.java
+++ src/main/java/org/nzip/Test.java
@@ -3,10 +3,11 @@ package org.nzip;
 import java.io.File;
 
 public class Test {
+	int testvar;
 public static void main(String[] args) {
 	System.out.println(File.separator);
 	
 	String s = "a\\b";
-	System.out.println(s.replaceAll("\\\\", "/"));
+	
 }
 }
